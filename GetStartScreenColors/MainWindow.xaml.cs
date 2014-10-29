498d0ae741b157d267ca5b49ffb4c3de5935d55c
VladimirAmiorkov <vladimir.amiorkov@telerik.com>
2014-10-29 09:30:55 +0200

    Added application

---
+++ GetStartScreenColors/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace GetStartScreenColors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGetColorsButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.SetColor(this.AccentColorResultRectangle, this.AccentColorResultTextBox, ImmersiveColors.ImmersiveStartSelectionBackground);
+            this.SetColor(this.MainColorResultRectangle, this.MainColorResultTextBox, ImmersiveColors.ImmersiveStartPrimaryText);
+            this.SetColor(this.BackgroundColorResultRectangle, this.BackgroundColorResultTextBox, ImmersiveColors.ImmersiveStartBackground);
+        }
+
+        private void SetColor(Rectangle rectangle, TextBox textBox, ImmersiveColors immersiveColor)
+        {
+            IntPtr pElementName = Marshal.StringToHGlobalUni(immersiveColor.ToString());
+            Color color = GetColor(pElementName);
+            rectangle.Fill = new SolidColorBrush(color);
+            textBox.Text = color.ToString();
+        }
+
+        private static Color GetColor(IntPtr pElementName)
+        {
+            var colourset = StarScreenColorsHelper.GetImmersiveUserColorSetPreference(false, false);
+            uint type = StarScreenColorsHelper.GetImmersiveColorTypeFromName(pElementName);
+            Marshal.FreeCoTaskMem(pElementName);
+            uint colourdword = StarScreenColorsHelper.GetImmersiveColorFromColorSetEx((uint)colourset, type, false, 0);
+            byte[] colourbytes = new byte[4];
+            colourbytes[0] = (byte)((0xFF000000 & colourdword) >> 24); // A
+            colourbytes[1] = (byte)((0x00FF0000 & colourdword) >> 16); // B
+            colourbytes[2] = (byte)((0x0000FF00 & colourdword) >> 8); // G
+            colourbytes[3] = (byte)(0x000000FF & colourdword); // R
+            Color color = Color.FromArgb(colourbytes[0], colourbytes[3], colourbytes[2], colourbytes[1]);
+            return color;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Windows-8-8.1-start-screen-colors-into-desktop-application
+Windows 8, 8.1 start screen colors into desktop application
 ==========================================================
 
 This repository shows a possible (unofficial) approach in retrieving the start screen colors (accent color, background color etc.) into a desktop WPF application using the .NET Framework.
+
+I was inspired to create such a sample project when researching the possible ways to achieve the desired behavior into a WPF application and make it into a Windows 8 start screen theme aware application. A similar approach is used in [this library](http://win8lib.codeplex.com/ "http://win8lib.codeplex.com/") and you can as easily download it and add its assembly in your project. The idea of this repository is to provide a way to retrieve the mentioned start screen colors without the need to add additional libraries to your project.
